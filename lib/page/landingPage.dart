640576a46bf116f86af96f12587f14e35c2ea928
reniel-deang <xexxdump@gmail.com>
2025-07-21 01:29:09 +0800

    Update main.dart and make folders for page and widgets

---
+++ lib/page/landingPage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class homePage extends StatefulWidget {
+  const homePage({super.key});
+
+  @override
+  State<homePage> createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      ),
+
+      body: Container(
+        child: Column(
+          children: [
+          ],
+        ),
+      ),
+    );
+  }
+}
